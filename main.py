0d18e757e8e186e5b172168b04b9e45cc3850670
alexandrurop <ropceana@gmail.com>
2025-07-16 21:31:03 +0300

    Actualizarea finala

--- main.py
+++ main.py
@@ -7,33 +7,20 @@ import sys
 import random
 
 
-# Variabile globale pentru dimensiunile tablei
-tabla_x = 0
-tabla_y = 0
 latime_celula = 24
 inaltime_celula = 24
 
-
 def detecteaza_stare_joc():
-    """
-    Returnează starea jocului Minesweeper analizând smiley face-ul:
-    - 'playing'
-    - 'won'
-    - 'lost'
-    - None dacă nu se poate determina
-    """
-
     screenshot = pyautogui.screenshot()
     screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
-    # Șabloane
     templates = {
         'playing': cv2.imread('smiley/play.png'),
         'won': cv2.imread('smiley/win.png'),
         'lost': cv2.imread('smiley/lose.png')
     }
 
-    # Căutăm fiecare șablon în screenshot
+
     for stare, template in templates.items():
         if template is None:
             print(f"[WARN] Șablonul {stare} nu a fost găsit!")
@@ -51,22 +38,22 @@ def gaseste_tabla_info(template_path="./templates/unknow.png", threshold=0.97):
     screenshot = pyautogui.screenshot()
     screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
     template = cv2.imread(template_path, cv2.IMREAD_COLOR)
-    h, w = template.shape[:2]
+
 
     rezultat = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
-    locuri = np.where(rezultat >= threshold)
+    locuri = np.where(rezultat >= threshold)#o lista cu toate coordonatele y1,y2 .. x1,x2
 
-    puncte = list(zip(*locuri[::-1]))  # (x, y)
+    puncte = list(zip(*locuri[::-1]))  # (x1, y1) (x2, y2)
 
     if not puncte:
         raise Exception("Nu s-au găsit celule.")
 
-    # Sortează punctele după Y, apoi X
-    puncte = sorted(puncte, key=lambda p: (p[1], p[0]))
+    
+    puncte = sorted(puncte, key=lambda p: (p[1], p[0])) 
 
-    # Grupuiește rândurile după distanța verticală
+
     randuri = []
-    dist_y = h // 2
+    dist_y = latime_celula // 2
     for p in puncte:
         gasit = False
         for rand in randuri:
@@ -77,7 +64,6 @@ def gaseste_tabla_info(template_path="./templates/unknow.png", threshold=0.97):
         if not gasit:
             randuri.append([p])
 
-    # Sortează fiecare rând după X (coloane)
     for rand in randuri:
         rand.sort(key=lambda p: p[0])
 
@@ -89,7 +75,7 @@ def gaseste_tabla_info(template_path="./templates/unknow.png", threshold=0.97):
     return nr_randuri, nr_coloane, colt_stanga_sus
 
 def identifica_celula(img_celula, templates_dir="templates"):
-    """Identifică tipul unei celule comparând cu template-uri"""
+    
     if not os.path.exists(templates_dir):
         print(f"[EROARE] Folderul {templates_dir} nu există!")
         return "necunoscut"
@@ -115,11 +101,10 @@ def identifica_celula(img_celula, templates_dir="templates"):
             max_score = score
             label_final = nume_template
     
+    #ia cel mai bun scor dintre toate
     return label_final if max_score > 0.85 else "necunoscut"
 
-def extract_cells(nr_randuri, nr_coloane):
-    """Extrage și salvează toate celulele din tabla de joc"""
-    global tabla_x, tabla_y, latime_celula, inaltime_celula
+def extract_cells(nr_randuri, nr_coloane,tabla_x, tabla_y):
     
     screenshot = pyautogui.screenshot()
     img = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
@@ -136,7 +121,7 @@ def extract_cells(nr_randuri, nr_coloane):
             x = tabla_x + coloana * latime_celula
             y = tabla_y + rand * inaltime_celula
             
-            celula = img[y:y+inaltime_celula, x:x+latime_celula]
+            celula = img[y:y+inaltime_celula, x:x+latime_celula] #o imagine fix cu patratelul 
             
             #path = f"cells/cell{rand}_{coloana}.png"
             #cv2.imwrite(path, celula)
@@ -149,10 +134,10 @@ def extract_cells(nr_randuri, nr_coloane):
     print(f"[INFO] Am extras {nr_randuri}x{nr_coloane} celule în folderul 'cells/'")
     return tabla_logica
 
-def pune_flags_sigure(tabla_logica):
-    global tabla_x, tabla_y, latime_celula, inaltime_celula
+def pune_flags_sigure(tabla_logica, tabla_x, tabla_y):
+
     schimbare1 = False
-    """Pune steaguri în celulele care sunt cu siguranță bombe"""
+    
     for rand in range(len(tabla_logica)):
         for coloana in range(len(tabla_logica[0])):
             val = tabla_logica[rand][coloana]
@@ -232,11 +217,6 @@ def pune_flags_sigure(tabla_logica):
                         px = tabla_x + x * latime_celula + latime_celula // 2
                         py = tabla_y + y * inaltime_celula + inaltime_celula // 2
 
-                        screenshot = pyautogui.screenshot()
-                        img = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-                        culoare = (0, 0, 255)
-                        cv2.imwrite("debug_flag.png", img)
-
                         pyautogui.rightClick(px, py)
                         schimbare1 = True
                         print(f"[FLAG] Flag la ({y}, {x})")
@@ -245,8 +225,8 @@ def pune_flags_sigure(tabla_logica):
                         tabla_logica[y][x] = "flag"
     return schimbare1
 
-def click_stanga_sigur(tabla_logica):
-    global tabla_x, tabla_y, latime_celula, inaltime_celula
+def click_stanga_sigur(tabla_logica, tabla_x, tabla_y):
+
     schimbare = False
     for rand in range(len(tabla_logica)):
         for coloana in range(len(tabla_logica[0])):
@@ -338,9 +318,8 @@ def click_stanga_sigur(tabla_logica):
                         time.sleep(0.05)
                         schimbare = True
 
-                        tabla_logica[y][x] = "empty"#? de ce y x si nu x y?
+                        tabla_logica[y][x] = "empty"
     return schimbare
-
 
 
 def click_random(tabla, tabla_x, tabla_y):
@@ -368,38 +347,32 @@ def click_random(tabla, tabla_x, tabla_y):
         sys.exit()
 
 def main(randuri, coloane,tabla_x,tabla_y):
-    print("[START] Bot Minesweeper...")
-    #click_random(tabla, tabla_x,tabla_y)
-    while True:
-        tabla = extract_cells(randuri, coloane)
-        schimbare1 = pune_flags_sigure(tabla)
 
-        #tabla = extract_cells(randuri, coloane)
-        schimbare2 = click_stanga_sigur(tabla)
+    while True:
+        tabla = extract_cells(randuri, coloane,tabla_x, tabla_y)
+        schimbare1 = pune_flags_sigure(tabla, tabla_x, tabla_y)
+
+        #tabla = extract_cells(randuri, coloane,tabla_x, tabla_y)
+        schimbare2 = click_stanga_sigur(tabla,tabla_x, tabla_y)
 
         if not (schimbare1 or schimbare2):
             stare = detecteaza_stare_joc()
             if stare == 'won':
-                print("Ai câștigaaat!!!!")
+                print("Am câștigaaat!!!!")
                 sys.exit()
             elif stare == 'playing':
-                print("Jocul este încă în desfășurare. Vom alege o pozitie random")
+                print("Jocul este încă în desfășurare. Voi alege o pozitie random")
                 click_random(tabla, tabla_x,tabla_y)
+            elif stare == 'lost':
+                print("Am pierdut :(((")
+                sys.exit()
 
         time.sleep(0.2)  
 
-    print("[DONE] Tură finalizată.")
-
 if __name__ == "__main__":
-    # Detectează tabla o singură dată
 
-    print("Timp pentru a accesa site-ul MineSweeper")
+    print("Timp pentru a accesa site-ul MineSweeper: 10 secunde")
     time.sleep(10)
-    
+
     randuri, coloane, (tabla_x, tabla_y) = gaseste_tabla_info()
-    # Actualizează dimensiunile celulelor
-    #latime_celula = w
-    #inaltime_celula = h
-    # Rulează botul
-   
     main(randuri, coloane,tabla_x,tabla_y)
